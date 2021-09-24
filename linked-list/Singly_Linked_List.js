99432b9281e86f06960b9bf1ea49f1445b7bbe0c
Rohan-Shakya <rohanshakya254@gmail.com>
2021-09-24 22:13:49 +0545

    added shift and unshift method to singly linked list

--- linked-list/Singly_Linked_List.js
+++ linked-list/Singly_Linked_List.js
@@ -63,4 +63,48 @@ class SinglyLinkedList {
     }
     return current;
   }
+
+  /*
+    =================== Shift Pseudo Code ==================
+    * If there are no nodes, return undefined
+    * Store the current head property in a variable
+    * Set the head property to be the current head's next property
+    * Decrement the length by 1
+    * Return the value of the node removed
+  */
+  shift() {
+    if (!this.head) return undefined;
+    let currentHead = this.head;
+    this.head = currentHead.next;
+    this.length--;
+    if (this.length === 0) {
+      this.tail = null;
+    }
+    return currentHead;
+  }
+
+  /*
+    =================== Shift Pseudo Code ==================
+    * This function should accept a value
+    * Create a new node using the value passed to the function
+    * If there is no head property on the list,
+      set the head and tail to be the newly created node
+    * Otherwise set the newly created node's next property to be the 
+      current head property on the list
+    * Set the head property on the list to be that newly created node
+    * Increment the length of the list by 1
+    * Return the linked list
+  */
+  unshift(val) {
+    let newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    this.length++;
+    return this;
+  }
 }
